11fbb87df4d16be11c02261216ae64cf744e1655
Craig Ford <craig_ford@icloud.com>
2022-09-06 19:11:48 -0400

    Created the Home component

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h1>My Vineyard and Wine Management Application</h1>
+        <hr/>
+        <p>
+            This application has a repository of Vineyards and their associated wines.  It is a management system so vineyards and their wines can be created, updated or deleted.  Please click on the Vineyards button to see the current repository of Vineyards.
+        </p>
+    </div>
+  )
+}
+
+export default Home
