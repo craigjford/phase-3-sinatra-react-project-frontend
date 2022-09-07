9b4ca6177c0e94429b6356e129e921f1e717d17a
Craig Ford <craig_ford@icloud.com>
2022-09-07 15:25:03 -0400

    coded Vineyard component and added Wine component

--- src/components/Vineyard.js
+++ src/components/Vineyard.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import Wines from './Wines';
 
 const Vineyard = () => {
+  const [vineyard, setVineyard] = useState({
+    wines: []
+  })
+  const [createWineForm, setCreateWineForm] = useState(false)
+
+  const params = useParams();
+
+  useEffect(() => {
+    fetch(`http://localhost:9292/vineyards/${params.id}`)
+       .then(res => res.json())
+       .then(data => {
+           console.log(data)
+           setVineyard(data)
+       })   
+  }, [params.id])
+
+  const wines = vineyard.wines.map(wine => <Wines key={wine.id} wine={wine} />)
+
   return (
-    <div>Vineyard</div>
+    <div>
+        <br />
+        <h2>{vineyard.name}</h2>
+        <hr/>
+        <h3>Wines:</h3>
+        <br />
+        {wines}
+        <br />
+    </div>
   )
 }
 
